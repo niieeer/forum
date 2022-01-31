70606b878e31a851230e53812fdf073181d3ca50
niieeer <clement0201@icloud.com>
2022-01-31 13:28:11 +0100

    Article et début User

--- index.php
+++ index.php
@@ -4,12 +4,11 @@ namespace App;
 
 require_once('vendor/autoload.php');
 
-use App\Entity\Book;
-use App\Entity\Visitor;
 use Router\Router;
 
 $router = new Router($_GET['url']);
 
+// Article
 $router->get("/", "App\Controller\AppController@index");
 $router->get("/add", "App\Controller\ArticleController@showForm");
 $router->post("/add", "App\Controller\ArticleController@addArticle");
@@ -18,10 +17,7 @@ $router->get("/modify/:id", "App\Controller\ArticleController@modifyArticle");
 $router->post("/modify/:id", "App\Controller\ArticleController@sendModifyArticle");
 $router->get("/delete/:id", "App\Controller\ArticleController@deleteArticle");
 
-
+// User
 $router->get("/adduser", "App\Controller\UserController@add");
-
-
-
 
 $router->run();
